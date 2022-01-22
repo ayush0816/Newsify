eb98daec16a70a06f55b5ef69b2439a92ec4d0bc
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2022-01-22 18:38:59 +0530

    Updates

---
+++ precache-manifest.f43a82a6ddb040a91f2ef817d7b9e124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f07d1a06f32230056dfc6652db19eaa",
+    "url": "/Newsify/index.html"
+  },
+  {
+    "revision": "bab9a4e2fb2c1b7babc9",
+    "url": "/Newsify/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "3b8892bd3d5f4abece2a",
+    "url": "/Newsify/static/js/2.f58be67f.chunk.js"
+  },
+  {
+    "revision": "fe8cd0fe695154e471ee",
+    "url": "/Newsify/static/js/3.adf0ca59.chunk.js"
+  },
+  {
+    "revision": "bab9a4e2fb2c1b7babc9",
+    "url": "/Newsify/static/js/main.3bf98da9.chunk.js"
+  },
+  {
+    "revision": "32133283391daf9130d4",
+    "url": "/Newsify/static/js/runtime~main.de473a42.js"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/Newsify/static/media/loading.3f259006.gif"
+  }
+]);
